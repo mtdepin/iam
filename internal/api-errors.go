377d6790813ebdd17f5bd813963660eb871ab19a
Liyechen <liyechen@maitian.com>
2022-03-03 18:23:06 +0800

    delete

--- internal/api-errors.go
+++ internal/api-errors.go
@@ -1,19 +1,4 @@
-// Copyright (c) 2015-2021 MinIO, Inc.
-//
-// This file is part of MinIO Object Storage stack
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 package internal
 
@@ -24,7 +9,6 @@ import (
 	"fmt"
 	"mt-iam/internal/auth"
 	"mt-iam/internal/crypto"
-	"mt-iam/internal/dns"
 	"mt-iam/internal/event"
 	"mt-iam/internal/hash"
 	"mt-iam/logger"
@@ -1912,12 +1896,6 @@ func toAPIErrorCode(ctx context.Context, err error) (apiErr APIErrorCode) {
 	if apiErr != ErrNone {
 		// If there was a match in the above switch case.
 		return apiErr
-	}
-
-	// etcd specific errors, a key is always a bucket for us return
-	// ErrNoSuchBucket in such a case.
-	if err == dns.ErrNoEntriesFound {
-		return ErrNoSuchBucket
 	}
 
 	switch err.(type) {
--- internal/dns/etcd_dns.go
+++
@@ -1,9 +0,0 @@
-package dns
-
-import "errors"
-
-// ErrNoEntriesFound - Indicates no entries were found for the given key (directory)
-var ErrNoEntriesFound = errors.New("No entries found for this key")
-
-// ErrDomainMissing - Indicates domain is missing
-var ErrDomainMissing = errors.New("domain is missing")
