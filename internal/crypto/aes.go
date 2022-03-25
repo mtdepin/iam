7285e072cd4372c49ed676c2315089ee483b7c38
huangshijie <huang_shi_jie@qq.com>
2022-03-25 14:41:00 +0800

    feature: 去掉加密

--- internal/crypto/aes.go
+++ internal/crypto/aes.go
@@ -4,9 +4,6 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/base64"
-	"fmt"
-	"mt-iam/pkg/logger"
 )
 
 const (
@@ -40,35 +37,37 @@ const (
 
 func PasswordEncrypt(password string) string {
 	// 不对空字符串加密
-	if password == "" {
-		return password
-	}
-	xpass, err := aesEncrypt([]byte(password), []byte(salt))
-	if err != nil {
-		logger.Error("密码加密失败", err)
-		return ""
-	}
-	pass64 := base64.StdEncoding.EncodeToString(xpass)
-	return pass64
+	//if password == "" {
+	//	return password
+	//}
+	//xpass, err := aesEncrypt([]byte(password), []byte(salt))
+	//if err != nil {
+	//	logger.Error("密码加密失败", err)
+	//	return ""
+	//}
+	//pass64 := base64.StdEncoding.EncodeToString(xpass)
+	//return pass64
+	return password
 }
 
 func PasswordDecrypt(password string) string {
 	// 不对空字符串解密
-	if password == "" {
-		return password
-	}
-	bytesPass, err := base64.StdEncoding.DecodeString(password)
-	if err != nil {
-		logger.Error("密码解密失败", err)
-		return ""
-	}
-
-	t, err := aesDecrypt(bytesPass, []byte(salt))
-	if err != nil {
-		logger.Error("密码解密失败", err)
-		return ""
-	}
-	return fmt.Sprintf("%s", t)
+	//if password == "" {
+	//	return password
+	//}
+	//bytesPass, err := base64.StdEncoding.DecodeString(password)
+	//if err != nil {
+	//	logger.Error("密码解密失败", err)
+	//	return ""
+	//}
+	//
+	//t, err := aesDecrypt(bytesPass, []byte(salt))
+	//if err != nil {
+	//	logger.Error("密码解密失败", err)
+	//	return ""
+	//}
+	//return fmt.Sprintf("%s", t)
+	return password
 }
 
 //@brief:填充明文
