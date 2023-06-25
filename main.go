173f2afad601e4188bc0548e4207770b4884711e
xiaoxiao <yechenli2009@gmail.com>
2023-06-25 15:26:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -45,42 +45,6 @@ func main() {
 
 	//包装的handler，处理定义之外的请求
 	wrappedHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 拦截 RequestURI 前缀 为 "/claim" 的请求
-		//if strings.Contains(r.RequestURI, "/claim") {
-		//	//把前缀去掉
-		//	r.RequestURI = strings.Replace(r.RequestURI, "/claim", "", 1)
-		//	token := internal2.MustGetClaimsFromToken(r)
-		//	// todo 处理异常结果返回
-		//	if token != nil {
-		//		//写入返回结果
-		//		w.Header().Set(http2.ContentType, "content-type/json")
-		//		w.WriteHeader(200)
-		//		result, _ := json.Marshal(token)
-		//		_, _ = w.Write(result)
-		//		w.(http.Flusher).Flush()
-		//		return
-		//	}
-		//	w.Header().Set(http2.ContentType, "content-type/json")
-		//	w.WriteHeader(200)
-		//	_, _ = w.Write([]byte(""))
-		//	w.(http.Flusher).Flush()
-		//	return
-		//} else
-		//if strings.Contains(r.RequestURI, "/auth") {
-		//	r.RequestURI = strings.Replace(r.RequestURI, "/auth", "", 1)
-		//	r.URL.Path = strings.Replace(r.URL.Path, "/auth", "", 1)
-		//
-		//	//r.Host = "192.168.1.135:9000"
-		//	internal2.IsAllowed(w, r)
-		//	return
-		//} else
-		//if strings.Contains(r.RequestURI, "/validateSignature") {
-		//	r.RequestURI = strings.Replace(r.RequestURI, "/validateSignature", "", 1)
-		//	r.URL.Path = strings.Replace(r.URL.Path, "/validateSignature", "", 1)
-		//
-		//	internal2.ValidateSignature(w, r)
-		//	return
-		//}
 		// Handle request using passed handler.
 		//正常请求
 		router.ServeHTTP(w, r)
